87eb970a94b15a548eeef2f33ae58f9d01c816c6
Maverick <embrymaverick@gmail.com>
2021-02-06 23:01:41 -0700

    Help file format

--- Commands/help.js
+++ Commands/help.js
@@ -1,4 +1,6 @@
-const { prefix } = require('../config.json');
+const {
+	prefix,
+} = require('../config.json');
 
 module.exports = {
 	name: 'help',
@@ -8,14 +10,20 @@ module.exports = {
 	cooldown: 5,
 	execute(message, args) {
 		const data = [];
-		const { commands } = message.client;
+		const {
+			commands,
+		} = message.client;
 
 		if (!args.length) {
-			data.push('Here\'s a list of all my commands:');
-			data.push(commands.map(command => command.name).join(', '));
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-			return message.author.send(data, { split: true })
+			data.push('Here\'s a list of all my commands:');
+			data.push(commands.map(command => '**' + command.name + '**').join(', '));
+			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!\n`);
+			data.push('*Contribute here: [https://github.com/Keagnn/TechSpaBot]*');
+
+			return message.author.send(data, {
+				split: true,
+			})
 				.then(() => {
 					if (message.channel.type === 'dm') return;
 					message.reply('I\'ve sent you a DM with all my commands!');
@@ -35,12 +43,13 @@ module.exports = {
 
 		data.push(`> Name: ${command.name}`);
 
-
 		if (command.description) data.push(`> Description: ${command.description}`);
 		if (command.usage) data.push(`> Usage: ${prefix}${command.name} ${command.usage}`);
 
 		data.push(`> Cooldown: ${command.cooldown || 3} second(s)`);
 
-		message.channel.send(data, { split: true });
+		message.channel.send(data, {
+			split: true,
+		});
 	},
 };
